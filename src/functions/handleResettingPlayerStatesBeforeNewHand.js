303f95167ff2db41d8e2892ae224f813f1c77e03
David Bachmann <dabbi@dabbi.is>
2017-04-25 21:49:41 +0000

    Not working

--- src/functions/handleResettingPlayerStatesBeforeNewHand.js
+++ src/functions/handleResettingPlayerStatesBeforeNewHand.js
@@ -1,26 +1,12 @@
-import update from 'immutability-helper'
-
 /**
  * Handles resetting player states before a new hand
  */
-// export default function handleResettingPlayerStatesBeforeNewHand(players) {
-//   players.forEach(player => {
-//     return update(player, {
-//       isAllIn: { $set: false },
-//       hasActed: { $set: false },
-//       hasFolded: { $set: false },
-//     })
-//   })
-//
-//   return players
-// }
 
 export default function handleResettingPlayerStatesBeforeNewHand(players) {
-  let _x = players
-  for (var i = 0; i < _x.length; i++) {
-    _x[i].hasActed = false
-    _x[i].isAllIn = false
-    _x[i].hasFolded = false
+  for (var i = 0; i < players.length; i++) {
+    players[i].hasActed = false
+    players[i].isAllIn = false
   }
-  return _x
+
+  return players
 }
--- src/redux/modules/gameManager.js
+++ src/redux/modules/gameManager.js
@@ -283,7 +283,6 @@ export default function reducer(state = initialState, action) {
           state.players,
           state.nextPlayerToAct,
           state.highestCurrentBettor,
-          state.positions,
         ),
       })
     }
